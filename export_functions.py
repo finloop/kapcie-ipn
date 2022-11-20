273ee5b479fe7d23763a29d9e295b80544530416
Hubert <hub.mazur99@gmail.com>
2022-11-20 01:18:54 +0100

    Kolorowanie XD

--- export_functions.py
+++ export_functions.py
@@ -1,17 +1,17 @@
 # pip install fpdf
+# pip install docx
+# pip install numpy
+
 from fpdf import FPDF
 from docx import Document
 import numpy as np
-from docx.shared import Inches
-import aspose.words as aw
-import io
-from docx2pdf import convert
+from docx.shared import RGBColor
+import fpdf
 
 
-def write_docx(json_data, output_filename):
+def write_docx(json_data, output_filename, highlight_correct=False):
 
     document = Document()
-
     ans_idxes = ['a', 'b', 'c', 'd']
 
     for key_idx in json_data.keys():
@@ -24,8 +24,12 @@ def write_docx(json_data, output_filename):
                                    np.clip(len(json_data[key_idx]['False_answers']) + 1, 2, 4), replace=False)
 
         for i, answer in enumerate(answers):
-            p.add_run(f'\n\t{ans_idxes[i]}) {answer}')
-
+            if highlight_correct and (answer == random_correct[0]):
+                run = p.add_run(f'\n\t{ans_idxes[i]}) {answer}')
+                run.font.color.rgb = RGBColor(0x00, 0xFF, 0x00)
+            else:
+                run = p.add_run(f'\n\t{ans_idxes[i]}) {answer}')
+                run.font.color.rgb = RGBColor(0x00, 0x00, 0x00)
     document.add_page_break()
 
     docx_fname = output_filename + '.docx'
@@ -33,7 +37,7 @@ def write_docx(json_data, output_filename):
     document.save(docx_fname)
 
 
-def write_pdf(json_data, output_filename):
+def write_pdf(json_data, output_filename, highlight_correct=False):
 
     pdf = FPDF()
 
@@ -42,6 +46,9 @@ def write_pdf(json_data, output_filename):
     pdf.add_page()
 
     pdf.set_font("Arial", size=15)
+
+    help(pdf.set_font)
+    help(fpdf.fpdf.FPDF)
 
     for key_idx in json_data.keys():
         pdf.set_font("Arial", size=15)
@@ -55,10 +62,14 @@ def write_pdf(json_data, output_filename):
 
         pdf.set_font("Arial", size=10)
         for i, answer in enumerate(answers):
-            pdf.cell(40, 7, txt=f'\n{ans_idxes[i]}) {answer}',
-                     ln=2, align='L')
-
-
+            if answer == random_correct[0] and highlight_correct:
+                pdf.set_text_color(0, 255, 0)
+                pdf.cell(40, 7, txt=f'\n{ans_idxes[i]}) {answer}',
+                         ln=2, align='L')
+                pdf.set_text_color(0, 0, 0)
+            else:
+                pdf.cell(40, 7, txt=f'\n{ans_idxes[i]}) {answer}',
+                         ln=2, align='L')
     pdf_fname = output_filename + '.pdf'
 
     pdf.output(pdf_fname)
@@ -78,5 +89,5 @@ if __name__ == '__main__':
 
                    }
 
-    write_docx(sample_dict, '2')
-    write_pdf(sample_dict, '2')
+    write_docx(sample_dict, '2', highlight_correct=True)
+    write_pdf(sample_dict, '2', highlight_correct=True)
